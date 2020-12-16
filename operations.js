a4963a26079575261152bfe221e33b9a3ca79ea3
Mohamed Sabry <mohamed20191705017@cis.asu.edu.org>
2020-12-16 10:36:06 +0200

    Using node modules for database operations

---
+++ operations.js
@@ -0,0 +1,47 @@
+//encapsulation of the operations of the database 
+const assert=require('assert');
+
+
+
+exports.insertDocument=(db, document, collection,callback)=>{
+const coll=db.collection(collection);
+coll.insert(document, (err,result)=>{
+assert.equal(err,null);
+console.log('Inserted:'+result.result.n +'documents into the collection ');
+callback(result);
+
+
+});
+
+};
+
+exports.findDocument=(db,collection,callback)=>{
+    //finds all the docs 
+    const coll=db.collection(collection);
+    coll.find({}).toArray((err,docs)=>{
+assert.equal(err,null);
+callback(docs);
+
+    });
+};
+exports.removeDocument=(db, document, collection,callback)=>{
+    const coll=db.collection(collection);
+    assert.equal(err,null);
+    coll.deleteOne(document, (err, result)=>{
+        assert.equal(err,null);
+        console.log(`Removed the document ${document}`);
+        callback(result);
+
+    });
+
+    
+};
+exports.updateDocument=(db, document, update, collection,callback)=>{
+     const coll=db.collection(collection);
+     assert.equal(err,null);
+     coll.updateOne(document, {$set:update},null,(err,result)=>{
+        assert.equal(err,null);
+        console.log('Updated the document with  ', update );
+        
+     });
+};
